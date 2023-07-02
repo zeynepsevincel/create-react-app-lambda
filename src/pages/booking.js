fcb08fdcb1b3922295203938a56a6ec53a86c2a8
Zeynep Buse Sevincel <zsevi066@uottawa.ca>
2023-07-02 16:01:13 -0400

    fml

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import RegistrationForm from './components/registrationform'
+import RegistrationForm from '../components/registrationform'
 
 
 
