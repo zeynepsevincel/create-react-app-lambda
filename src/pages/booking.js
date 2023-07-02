59608dfa3adb421abe477a63238eebd54376114b
Zeynep Buse Sevincel <zsevi066@uottawa.ca>
2023-07-02 14:27:05 -0400

    working on booking

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+
+
+
+const imag1 = "https://housing.com/news/wp-content/uploads/2022/11/hotel-room-design-compressed-1.jpg";
 
 
 const Booking = () => {
@@ -6,7 +11,12 @@ const Booking = () => {
 return (
 	<div className="contact">
 
-
+<Parallax bgImage={imag1} strength={500}>
+      <div style={{ height: 500 }}>
+        <h1 style={{marginTop:"10%", fontWeight:"bold", fontSize:"100px", marginLeft:"34%", color:"white",}}>RESERVATIONS</h1>
+		
+      </div>
+    </Parallax>
 
 
 	</div>
